6ad2ee6ce28ff48e5c3f4a3c1f483916a70a477a
valensarrr <101572268+valensarrr@users.noreply.github.com>
2022-06-06 10:51:02 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+
+  stdio_init_all();
+  gpio_init(1);
+  gpio_init(25);
+  gpio_set_dir (1, false);
+  gpio_set_dir (25, true);
+
+  while (true) {
+
+    char a = gpio_get(1);
+    char b = gpio_get(25);
+
+    if (a==1) {
+
+      if(b == 1) {
+        gpio_put(25, false);
+      }
+      else {
+        gpio_put(25, true);
+      }
+    }
+
+    sleep_ms(500);
+  }
+}
